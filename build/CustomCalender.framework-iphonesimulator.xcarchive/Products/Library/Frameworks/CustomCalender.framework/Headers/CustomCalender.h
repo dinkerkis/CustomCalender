7dbf7c0be71300405244ce146cb705818a232e1e
Minakshi Bhardwaj <minakshibhardwaj.kis@gmail.com>
2021-05-26 13:59:45 +0530

    Initial Commit

---
+++ build/CustomCalender.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/CustomCalender.framework/Headers/CustomCalender.h
@@ -0,0 +1,18 @@
+//
+//  CustomCalender.h
+//  CustomCalender
+//
+//  Created by Mac48 on 20/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomCalender.
+FOUNDATION_EXPORT double CustomCalenderVersionNumber;
+
+//! Project version string for CustomCalender.
+FOUNDATION_EXPORT const unsigned char CustomCalenderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomCalender/PublicHeader.h>
+
+
