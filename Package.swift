a97bd866c42f3d0ecc8be3e0dc3421d15e5c6978
Minakshi Bhardwaj <minakshibhardwaj.kis@gmail.com>
2021-05-26 11:28:23 +0530

    Updates

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
             name: "CustomCalender",
             targets: ["CustomCalender"]),
     ],
-    dependencies: [
+//    dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
    
            // .package(url: "https://github.com/dinkerkis/CustomCalender.git", from: "1.0.0"),
-        ],
+//        ],
 
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
